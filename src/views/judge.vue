1d38e4c928a2141ae56c1c7963b9aead05d2afc8
sky1 <497363953@qq.com>
2022-11-13 18:51:58 +0800

    upload

--- src/views/judge.vue
+++ src/views/judge.vue
@@ -1,4 +1,17 @@
-<script setup></script>
+<script setup>
+import { isLogin } from "../api";
+import { useRouter } from "vue-router";
+import { useUserStore } from "../store";
+
+const router = useRouter();
+onMounted(() => {
+  isLogin(() => {
+    if (useUserStore().role !== "judge") {
+      router.push("/upload");
+    }
+  });
+});
+</script>
 
 <template>
   <div>
